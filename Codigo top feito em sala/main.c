50b36c887d56025955d6dd6c8e677b624a8c34fc
M4tt.Pizz4 <100164090+m4ttpizz4@users.noreply.github.com>
2024-10-18 09:33:35 -0300

    Add files via upload

--- Codigo top feito em sala/main.c
+++ Codigo top feito em sala/main.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define TAR_VET 100000
+#define TAM_VET 100000
 #define QTD_THREADS 4
 
 int id[QTD_THREADS];
-int vetor[TAR_VET];
+int vetor[TAM_VET];
 int resultado = 0;
 
-void * funcao(void * args)
-{
+pthread_mutex_t mutex;
+
+void * funcao(void * args){
     int id = *((int *)args);
-    int intervalo = TAR_VET/QTD_THREADS;
+    int intervalo = TAM_VET/QTD_THREADS;
     int soma = 0;
-    for(int i = id*intervalo; i < (id+1)*intervalo; i++)
-    {
+    for(int i = id*intervalo; i < (id+1)*intervalo; i++){
         soma += vetor[i];
     }
+    pthread_mutex_lock(&mutex);
     resultado += soma;
+    pthread_mutex_unlock(&mutex);
 }
 
-int main()
-{
-    for(int i = 0; i< TAR_VET; i++)
-    {
-        vetor[i] = i;    
+int main(){
+    
+    pthread_mutex_init(&mutex, NULL);
+    for(int i = 0; i < TAM_VET; i++){
+        vetor[i] = i;
     }
-    for(int i = 0; i < QTD_THREADS; i++)
-    {
+    for(int i = 0; i < QTD_THREADS; i++){
         id[i] = i;
     }
-    
     pthread_t threads[QTD_THREADS];
-    
-    for(int i = 0; i < QTD_THREADS; i++)
-    {
+    for(int i = 0; i < QTD_THREADS; i++){
         pthread_create(&threads[i], NULL, funcao, &id[i]);
     }
-    for(int i = 0; i < QTD_THREADS; i++)
-    {
+    for(int i = 0; i < QTD_THREADS; i++){
         pthread_join(threads[i], NULL);
     }
+    pthread_mutex_destroy(&mutex);
     
     printf("Resultado: %d\n", resultado);
     
